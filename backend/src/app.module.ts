3cbe43aa7af4320a039bb761be2e06ab3507d9c4
RomainM27 <romain.mathieu96@gmail.com>
2024-02-11 23:17:15 +0100

    chore: remove back template

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { TasksModule } from './tasks/tasks.module';
 import { SectionsModule } from './sections/sections.module';
 import { BoardsModule } from './boards/boards.module';
@@ -8,7 +7,7 @@ import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [TasksModule, SectionsModule, BoardsModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
